33c2302bdcaaca90dc47a4d8b68aef00723d0f8d
Lewis Sneddon <lewis.sneddon@icloud.com>
2021-01-10 09:10:19 +0000

    validation on submit

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { UserSettings } from '../data/user-settings';
 
@@ -22,6 +23,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm): void {
+    console.log('in onSubmit: ', form.valid);
   }
 
 }
